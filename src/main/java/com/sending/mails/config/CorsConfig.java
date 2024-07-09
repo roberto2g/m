8f564a4676801d522a564356338296d1c7020ebf
marcosLopaz1 <=>
2024-07-09 15:31:18 -0300

    ultimo 2

--- src/main/java/com/sending/mails/config/CorsConfig.java
+++ src/main/java/com/sending/mails/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("/**")
+                        .allowedOrigins("http://localhost:4200/")
                         .allowedMethods("GET","POST", "OPTIONS")
                         .allowedHeaders("Content-Type");
             }
